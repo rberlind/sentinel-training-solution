ae7101121421380fab65f8698333d3fedf44ae1f
rberlind <roger@hashicorp.com>
2019-02-07 15:50:14 -0500

    add path to vault example

--- vault-auth-methods/main.tf
+++ vault-auth-methods/main.tf
@@ -2,6 +2,11 @@ variable "type" {
   description = "type of Vault auth method"
 }
 
+variable "path" {
+  description = "path to Vault auth method"
+}
+
 resource "vault_auth_backend" "test" {
   type = "${var.type}"
+  path = "${var.path}"
 }
