f793cb5f8b18b836f5aab65ec1033b756486a439
rberlind <roger@hashicorp.com>
2019-02-08 14:38:17 -0500

    two certs

--- acm_certs/main.tf
+++ acm_certs/main.tf
@@ -1,7 +1,15 @@
-variable "domain" {
-  description = "domain of Amazon Certificate Manager certificate"
+variable "domain_1" {
+  description = "domain of first Amazon Certificate Manager certificate"
 }
 
-data "aws_acm_certificate" "hashidemos" {
-  domain = "${var.domain}"
+variable "domain_2" {
+  description = "domain of second Amazon Certificate Manager certificate"
+}
+
+data "aws_acm_certificate" "cert_1" {
+  domain = "${var.domain_1}"
+}
+
+data "aws_acm_certificate" "cert_2" {
+  domain = "${var.domain_2}"
 }
