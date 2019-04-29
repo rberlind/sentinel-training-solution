f19c035b4aba03716420e1460b2ec36f2ddf099c
Roger Berlind <roger@hashicorp.com>
2019-04-29 16:16:48 -0400

    revert back to initialize_params

--- gcp-instance/main.tf
+++ gcp-instance/main.tf
@@ -21,20 +21,6 @@ variable "image" {
   default = "debian-cloud/debian-9"
 }
 
-variable "labels" {
-  type = "map"
-  default = {
-    environment = "demo"
-    app = "demo"
-    ttl = "24h"
-  }
-}
-
-variable "os_pd_ssd_size" {
-  description = "Size of OS disk in GB"
-  default = "10"
-}
-
 provider "google" {
   credentials = "${var.gcp_credentials}"
   project     = "${var.gcp_project}"
@@ -46,14 +32,10 @@ resource "google_compute_instance" "demo" {
   machine_type = "${var.machine_type}"
   zone         = "us-east1-b"
 
- /* boot_disk {
+ boot_disk {
     initialize_params {
       image = "${var.image}"
     }
-  }*/
-    
-  boot_disk {
-    source = "${google_compute_disk.os-disk.name}"
   }
 
   network_interface {
@@ -64,21 +46,4 @@ resource "google_compute_instance" "demo" {
     }
   }
 
-}
-
-resource "random_string" "random-identifier" {
-  length = 4
-  special = false
-  upper = false
-  lower = true
-  number = true
-}
-
-resource "google_compute_disk" "os-disk" {
-  name   = "os-disk-${random_string.random-identifier.result}"
-  type   = "pd-ssd"
-  image  = "${var.image}"
-  labels = "${var.labels}"
-  size   = "${var.os_pd_ssd_size}"
-  zone   = "us-east1-b"
 }
